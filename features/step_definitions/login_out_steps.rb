306c8f4732ace4404af74ed64e3b5d011e2fa162
spacemonkeythe <mladen.meseldzija@gmail.com>
2015-02-05 21:15:39 +0100

    Add scenarios for bad login.

--- features/step_definitions/login_out_steps.rb
+++ features/step_definitions/login_out_steps.rb
@@ -1,47 +1,64 @@
 Given(/^I am registrated user$/) do
   @user = FactoryGirl.create(:user)
-  visit signup_path
-  fill_in "Name", with: @user.name
-  fill_in "Email", with: @user.email
-  fill_in "Password", with: @user.password
-  click_button "Create my account"
-end
-
-Given(/^I am not loged in$/) do
-  find_link("Account").click
-  find_link("Log out").click
 end
 
 When(/^I try to log in without email and password$/) do
+  visit login_path
   click_button "Create my account"
 end
 
 When(/^I try to log in without password$/) do
+  visit login_path
   fill_in "Email", with: @user.email
   click_button "Create my account"
 end
 
 When(/^I try to log in without email$/) do
+  visit login_path
 fill_in "Password", with: @user.password
   click_button "Create my account"
 end
 
 When(/^I try to log in with wrong email$/) do
+  visit login_path
   fill_in "Email", with: "example@exaple.com"
   fill_in "Password", with: @user.password
   click_button "Create my account"
 end
 
 When(/^I try to log in with wrong password$/) do
+  visit login_path
   fill_in "Email", with: @user.email
   fill_in "Password", with: "1234548907890"
   click_button "Create my account"
 end
 
 When(/^I cancel loging in$/) do
+  visit login_path
   click_link "Cancel"
 end
 
 Then(/^I should be on the home page$/) do
-    expect(page).to have_content("Welcome to Twitter rip-off app")
+  expect(page).to have_content("Welcome to Twitter rip-off app")
 end
+
+When(/^I provide valid credentials$/) do
+  visit login_path
+  fill_in "Email", with: @user.email
+  fill_in "Password", with: @user.password
+  click_button "Create my account"
+end
+
+Then(/^I should be loged in$/) do
+  page.find("a", text: "Account")
+end
+
+When(/^I logout$/) do
+  find_link("Account").click
+  find_link("Log out").click
+end
+
+Then(/^I should be loged out$/) do
+  find("a", text: "Log in")
+end
+
