ad4225f7b8824d718f2bf457b883fe3287eefd92
spacemonkeythe <mladen.meseldzija@gmail.com>
2015-02-21 17:23:20 +0100

    Fix static pages controller specs.

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,11 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe StaticPagesController, :type => :controller do
-
+ 
   describe "GET home" do
-    it "returns HTTP success" do
-      get :home
-      expect(response).to have_http_status(:success)
+    before do
+      @tweet = double(Tweet)
+      @feed_items = double("Tweets")
+      @user = double(User)
+    end
+
+    context "user is loged in" do
+      before do
+        allow(controller).to receive(:current_user) { @user }
+        allow(@user).to receive(:logged_in?) { true }
+        allow(@user).to receive_message_chain(:tweets, :build) { @tweet }
+        allow(@user).to receive_message_chain(:feed, :paginate) { @feed_items }
+      end
+
+      it "checks if the user is logged in" do
+        expect(@user.logged_in?).to eql(true)
+      end
+
+      it "returns HTTP success" do
+        get :home
+        expect(response).to have_http_status(:success)
+      end
+
+      it "assigns @tweet" do
+        get :home
+        expect(assigns(:tweet)).to eql(@tweet)
+      end
+
+      it "assigns @feed_items" do
+        get :home
+        expect(assigns(:feed_items)).to eql(@feed_items)
+      end
+    end
+
+    context "User is not logged in" do
+      before do
+        allow(@user).to receive(:logged_in?) { false }
+      end
+
+      it "checks if the user is logged in" do
+        expect(@user.logged_in?).to eql(false)
+      end
     end
 
     it "renders the home page" do
